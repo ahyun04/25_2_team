d618a9e01f4d70e1d371f4643dd13f2ec5fbc9c3
namsuhan0322 <rnatpakfl@gmail.com>
2025-07-25 12:18:07 +0900

    [Update] GameManager InitializeGame() 추가

--- 25_2_fishbattle/Assets/Scripts/Managers/GameManager.cs
+++ 25_2_fishbattle/Assets/Scripts/Managers/GameManager.cs
@@ -13,6 +13,15 @@ public class GameManager : SingletonMono<GameManager>
     protected override void Awake()
     {
         base.Awake();
+        InitializeGame();
+    }
+
+    // 게임 기본 설정
+    private void InitializeGame()
+    {
+        Application.targetFrameRate = 60;
+
+        // 더 생기면 추가 가능
     }
 
     #endregion

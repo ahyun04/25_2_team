2f32b1d6e923c5baf4feba0df5f857787ebbd180
namsuhan0322 <rnatpakfl@gmail.com>
2025-07-29 21:06:29 +0900

    [Fix] 테스트용 코드 수정

--- 25_2_fishbattle/Assets/Scripts/FishingGame/FishingMiniGame.cs
+++ 25_2_fishbattle/Assets/Scripts/FishingGame/FishingMiniGame.cs
@@ -87,8 +87,8 @@ public class FishingMiniGame : MonoBehaviour
         Debug.Log("낚시 시작... 물고기를 기다리는 중...");
 
         // 테스트 용
-        float waitTime = Random.Range(1f, 2f);
-        //float waitTime = Random.Range(5f, 15f);
+        //float waitTime = Random.Range(1f, 2f);
+        float waitTime = Random.Range(5f, 15f);
         yield return new WaitForSeconds(waitTime);
 
         Debug.Log("물고기가 찌를 물었다!");

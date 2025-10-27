dd0a1b2ff89fafd595c40ffeaad6030bf56b810e
namsuhan0322 <rnatpakfl@gmail.com>
2025-10-27 16:05:32 +0900

    [Feat] 씬 전환할때 해당 씬 skybox교체 추가 및 씬 어두워지는 부분 수정

---
+++ 25_2_fishbattle/Assets/Scripts/Skybox/SceneLightingSetup.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SceneLightingSetup : MonoBehaviour
+{
+    #region 레퍼런스
+    [Header("해당 씬 skybox")]
+    public Material sceneSkybox;
+    #endregion
+
+    #region 초기화
+    void Start()
+    {
+        if (SkyboxManager.Instance != null)
+        {
+            SkyboxManager.Instance.SetSkybox(sceneSkybox);
+        }
+        else
+        {
+            Debug.LogError("SkyboxManager가 씬에 존재하지 않습니다! 첫 씬에 SkyboxManager를 배치했는지 확인하세요.");
+
+            // 매니저가 없으면 일단 이 씬의 설정이라도 직접 적용
+            RenderSettings.skybox = sceneSkybox;
+            DynamicGI.UpdateEnvironment();
+        }
+    }
+
+    #endregion
+}
